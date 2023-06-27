24c479b08d2a8a280d365b8e72d5ceed35429067
djhoyos21 <deymer.hoyos21@gmail.com>
2023-06-27 18:43:47 -0500

    Commit initial section 4 course angular

---
+++ src/app/counter/counter.module.ts
@@ -0,0 +1,11 @@
+import { CounterComponent } from './components/counter/counter.component';
+import { NgModule } from '@angular/core';
+
+
+@NgModule({
+  imports: [],
+  exports: [CounterComponent],
+  declarations: [CounterComponent],
+  providers: [],
+})
+export class CounterModule { }
